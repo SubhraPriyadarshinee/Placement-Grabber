6a25419798d84cb946adeea173db184333bcd129
Reuse-Portal-Incture <109498172+Reuse-Portal-Incture@users.noreply.github.com>
2022-10-27 23:16:08 +0530

    glass css added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './App.css';
-import Appbody from './Appbody'
+import "./App.css";
+import Appbody from "./Appbody";
 
-const App= () => {
+const App = () => {
   return (
     <div className="App">
       <div className="main">
-      <Appbody/>
-
+        <div className="flex_container">
+          <div className="glass">
+            <h1 className="logo">
+              <a href="#">Subhra Singh</a>
+            </h1>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/Appbody.js
+++ src/Appbody.js
@@ -2,20 +2,11 @@ import "./App.css";
 
 function Appbody() {
   return (
-    <div className="main">
+      <div className="glass">
         <h1 className="logo">
           <a href="#">Subhra Singh</a>
         </h1>
-        {/* <div className="nav-wrap">
-          <nav className="main-nav" role="navigation">
-            <ul className="list-hover-slide unstyled">
-              <li>
-                <a href="#">About</a>
-              </li>
-            </ul>
-          </nav>
-        </div> */}
-    </div>
+      </div>
   );
 }
 
