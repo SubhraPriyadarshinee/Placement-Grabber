8b652a5f73f12e8621aa7e6d8c9381c66d593ded
SubhraPriyadarshinee <subhrapriyadarshinee@gmail.com>
2022-12-30 00:31:19 +0530

    double graph completed

--- src/line_chart.tsx
+++ src/line_chart.tsx
@@ -55,94 +55,122 @@ ChartJS.register(
 const options = {
   responsive: true,
   scales: {
+    x: {
+      grid: {
+        display: false
+      }
+    },
     y: {
       ticks: {
         callback: function (value) {
           return value;
         },
       },
+      grid: {
+        display: false
+      }
     },
   },
-  tension:3,
-  lineTension:0.4,
+  tension: 3,
+  lineTension: 0.4,
   legend: {
     display: true,
     labels: {
-        fontColor: '#333',
-        usePointStyle:true
+      fontColor: '#333',
+      usePointStyle: true
     }
-}
+  }
 };
 
-const labels = ["Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const labels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 export const data = {
   labels,
   datasets: [
     {
       fill: true,
       label: "Previous week",
-      data: [5,2,10,8,15,10,20,17,25],
+      data: [15, 2, 10, 8, 15, 10, 20, 17, 25],
       lineTension: 0.4,
-      borderColor: '#610C63'
+      borderColor: '#705AD1',
+      pointRadius: 0
+    },
+    {
+      fill: true,
+      label: "Income",
+      data: [17, 10, 17, 18, 26, 12, 23, 20, 25],
+      lineTension: 0.4,
+      borderColor: '#E79BE3',
+      pointRadius: 0
     }
   ],
 };
 const colors = [
-    'white',
-    '#F900BF',
-    '#9900F0',
-    'lime',
-    'green',
-    'teal',
-    'blue',
-    'purple',
-  ];
- 
+  'white',
+  '#705AD1',
+  '#705AD1',
+  'white',
+  '#E79BE3',
+  'teal',
+  'blue',
+  'purple',
+];
 
 
-function createGradient(ctx: CanvasRenderingContext2D, area: ChartArea) {
+
+function createGradient(ctx: CanvasRenderingContext2D, area: ChartArea,i) {
   var colorStart = colors[0];
-    var colorMid = colors[1];
-    var colorEnd = colors[2];
-  
-    
-  
-    const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top);
-  
-    gradient.addColorStop(0, colorStart);
-    gradient.addColorStop(0.5, colorMid);
-    gradient.addColorStop(1, colorEnd);
-  
-    return gradient;
+  var colorMid = colors[1];
+  var colorEnd = colors[2];
+  if(i==="Income"){
+     colorStart = colors[3];
+   colorEnd = colors[4];
   }
 
-  
+
+
+  const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top);
+
+ 
+  if(i==="Income"){
+    gradient.addColorStop(0, colorStart);
+    // gradient.addColorStop(0, colorMid);
+    gradient.addColorStop(0.9, colorEnd);
+ }else{
+  gradient.addColorStop(0, colorStart);
+  // gradient.addColorStop(0, colorMid);
+  gradient.addColorStop(0.45, colorEnd);
+ }
+
+  return gradient;
+}
+
 
 const LineChart = () => {
-    const chartRef = useRef<ChartJS>(null);
-    const [chartData, setChartData] = useState<ChartData<'bar'>>({
-        datasets: [],
-      });
-    useEffect(() => {
+  const chartRef = useRef<ChartJS>(null);
+  const [chartData, setChartData] = useState<ChartData<'bar'>>({
+    datasets: [],
+  });
+  useEffect(() => {
 
-        const chart = chartRef.current;
-        if (!chart) {
-            return;
-          }
-          const chartData = {
-            ...data,
-            datasets: data.datasets.map(dataset => ({
-              ...dataset,
-              backgroundColor: createGradient(chart.ctx, chart.chartArea)
-            })),
-          };
-          setChartData(chartData);
-    }, []);
+    const chart = chartRef.current;
+    if (!chart) {
+      return;
+    }
+    const chartData = {
+      ...data,
+      datasets: data.datasets.map(dataset => ({
+        ...dataset,
+        backgroundColor: createGradient(chart.ctx, chart.chartArea,dataset.label)
+        
+      })),
+    };
+    setChartData(chartData);
+  }, []);
   return (
     <div className="chart">
-        
+
       {/* <Line data={data} options={options} /> */}
-      <Chart ref={chartRef} type='line' data={chartData} />
+      <Chart ref={chartRef} type='line' data={chartData} options={options} />
     </div>
   );
 };
